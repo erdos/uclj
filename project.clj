602aff0039f6b92665ce2062ed3a097d6eeb0181
erdos <erdosj@gmail.com>
2021-12-31 09:53:29 +0100

    fix: extend reflectionconfig

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject uclj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Minimalist and fast Clojure interpreter"
+  :url "https://github.com/erdos/uclj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
