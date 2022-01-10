8c36435c64c6cd43ff37a538422ddca845aa2d2c
Janos Erdos <erdosj@gmail.com>
2022-01-10 22:41:59 +0100

    Update build-graal.sh

--- build-graal.sh
+++ build-graal.sh
@@ -6,7 +6,20 @@ lein uberjar
 
 JARFILE=`ls target/uclj-*-standalone.jar`
 
-$GRAALVM_HOME/bin/native-image \
+if [ -z "$GRAALVM_HOME" ]
+then
+    NATIVE_IMAGE="$GRAALVM_HOME/bin/native-image"
+else
+    NATIVE_IMAGE=native-image
+fi
+
+if ! command -v $NATIVE_IMAGE &> /dev/null
+then
+    echo "native-imagee could not be found"
+    exit 1
+fi
+
+$NATIVE_IMAGE \
     --no-fallback \
     -H:ReflectionConfigurationFiles=reflectconfig \
     --initialize-at-build-time \
