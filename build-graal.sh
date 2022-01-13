10784e85f38a9ef7c228ee7bf0897fd1d9623a79
Janos Erdos <erdosj@gmail.com>
2022-01-13 20:20:31 +0100

    feat: ci to publish compiler binary (#5)

--- build-graal.sh
+++ build-graal.sh
@@ -2,8 +2,7 @@
 
 set -euxo pipefail
 
-if [ -n "${GRAALVM_HOME-}" ]
-then
+if [ -n "${GRAALVM_HOME-}" ]; then
     NATIVE_IMAGE="$GRAALVM_HOME/bin/native-image"
 else
     NATIVE_IMAGE=native-image
@@ -11,13 +10,20 @@ fi
 
 command -v $NATIVE_IMAGE
 
-lein uberjar
+JARFILE=`ls target/uclj*-standalone.jar`
 
-JARFILE=`ls target/uclj-*-standalone.jar`
+if [ ! -f "$JARFILE" ]; then
+    lein uberjar
+else
+    echo "Jar file already exists!"
+fi
 
 $NATIVE_IMAGE \
     --no-fallback \
     -H:ReflectionConfigurationFiles=reflectconfig \
     --initialize-at-build-time \
     --allow-incomplete-classpath  \
-    -jar $JARFILE uclj
+    -jar $JARFILE \
+    uclj
+
+chmod a+x uclj
