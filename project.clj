c04f1771315b1941ec6450ddec5801eca5782ed0
erdos <erdosj@gmail.com>
2022-01-14 22:52:11 +0100

    feat: release v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uclj "0.1.0-SNAPSHOT"
+(defproject uclj "0.1.0"
   :description "Minimalist and fast Clojure interpreter"
   :url "https://github.com/erdos/uclj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
