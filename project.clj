fe7d0f54a4b8bdbece40d8f54de76f6352cba036
erdos <erdosj@gmail.com>
2022-04-19 22:04:03 +0200

    feat: bump clojure version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject uclj "0.1.4"
+(defproject uclj "0.1.5-SNAPSHOT"
   :description "Minimalist and fast Clojure interpreter"
   :url "https://github.com/erdos/uclj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.async "1.5.644"]
                  [org.clojure/core.cache "1.0.225"]
                  [org.clojure/core.logic "1.0.0"]
